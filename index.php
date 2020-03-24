bb0dffd16064179ddc51905c05f2ec0ebcc195a2
PorfirioDEH <60378253+PorfirioDEH@users.noreply.github.com>
2020-03-23 19:50:25 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ahorcado versión Bátiz</title>
+    <link rel="stylesheet" href="CSS/master.css">
+    <script src="JS/horcado.js"></script>
+</head>
+<body>
+    <h1>Ahorcado</h1>
+    <br>
+    <hr>
+    <br>
+    <canvas id="pantalla" width="960px" height="450px">
+        Tu navegador no soporta canvas
+    </canvas>
+    <br>
+    <hr>
+    <br>
+    <button  id="boton" type="reset" onclick="javascript:window.location.reload();">
+        Volver a Jugar
+    </button>
+</body>
+</html>
